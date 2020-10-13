538dce4211c76d8b99286c51c2a9a1a23879ab9c
abhilash puthukkudi <abhilashp44@gmail.com>
2020-10-13 13:27:02 +0530

    commit

--- comparison_operators.py
+++ comparison_operators.py
@@ -3,13 +3,13 @@ y=int(input("Enter value of Y "))
 if x>y :
     print(x," Grater Than ",y)
 elif x<y :
-    print(x," Less than Y")
+    print(x," Less than " , y)
 elif x>=y :
     print(x,"grater than or equal to ",y)
 elif x<=y :
     print(x,"is less than or equal to ",y)
 elif x==y :
-    print(x,"is eqal to y")
+    print(x,"is eqal to ",y)
 elif x!=y :
     print(x,"Equal to ",y)
 
