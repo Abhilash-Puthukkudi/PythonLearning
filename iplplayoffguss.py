57ad51353d2dc9ac04c985d0e9c736be228171ab
abhilash puthukkudi <abhilashp44@gmail.com>
2020-10-31 13:36:09 +0530

    guss game

---
+++ iplplayoffguss.py
@@ -0,0 +1,9 @@
+#guss ipl 2020 playoff teams 
+
+teams=["MI","DC","RCB","RR"]
+
+guss=input("Guss a Team?")
+if guss in teams:
+    print("wow!..",guss ,"is in IPL 2020 playoff")
+else:
+    print("sad..!",guss,"is not in IPL 2020 playoff")
