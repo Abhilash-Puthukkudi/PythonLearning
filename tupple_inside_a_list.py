4016dd3d198593e9b1f81fd87d2cc1ea5b5af8f5
abhilash puthukkudi <abhilashp44@gmail.com>
2020-10-31 16:21:01 +0530

    tupple inside a list

---
+++ tupple_inside_a_list.py
@@ -0,0 +1,8 @@
+#tupple inside a list
+
+locations=[] #list declared
+kerala=(34.0522,188.2438) #tupple
+kozhikode=(41.8781,87.6298) #tupple
+locations.append(kerala)# appending tupple to list
+locations.append(kozhikode) #appending tupple to list
+print(locations) # printing list
