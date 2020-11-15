3206cee28d46c9664aa79e273a0757a841bf6e62
abhilash puthukkudi <abhilashp44@gmail.com>
2020-11-15 20:53:25 +0530

    greatest

---
+++ greatest.py
@@ -0,0 +1,13 @@
+print("enter a , b ,c")
+a=input()
+b=input()
+c=input()
+if a>b :
+    if a>c:
+        print(a,"is grater")
+    else :
+        print(c,"is grater")
+elif b>c:
+     print(b," is grater")
+else:
+    print(c,"is grater")
